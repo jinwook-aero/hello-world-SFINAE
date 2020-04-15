a1b3ab05c4746421aa05eb05e58e8299811e5598
Jinwook Lee <jinwook.aero@gmail.com>
2020-04-14 23:59:52 -0700

    Example5 works with exact type matching

--- exampletest.cpp
+++ exampletest.cpp
@@ -10,30 +10,24 @@
 #include <iostream>
 #include <type_traits>
 
-// Example5: Compile failure
+// Example5: Compile success - Exact type matching
 template <typename T>
 class OddClass
 {
 public:
-	typename std::enable_if_t<std::is_integral_v<T>, bool>
-		is_odd(T i){ return bool(i % 2); };
-	//bool is_odd(T i);
+	typename std::enable_if_t<std::is_integral_v<T>, bool> is_odd(T i);
 };
 
-//template <typename T, typename Q=T>
-//typename std::enable_if_t<std::is_integral<T>::value, bool
-//OddClass<T>::is_odd(Q i){ return bool(i % 2); }
+template <typename T>
+typename std::enable_if_t<std::is_integral_v<T>, bool> 
+OddClass<T>::is_odd(T i)
+{ return bool(i % 2); };
 
 int main()
 {
 	int x = 1;
 	OddClass<int> theClass;
-	OddClass<char> wrongClass;
 	if(theClass.is_odd(x))
 		std::cout << "x is odd" << std::endl;
-	wrongClass.is_odd(x);
 	return 0;
 }
-
-// 1 > D:\Kindergarten\hello - world - SFINAE\example4.cpp(23, 1) : error C2244 : 'OddClass<T>::is_odd' : unable to match function definition to an existing declaration
-
